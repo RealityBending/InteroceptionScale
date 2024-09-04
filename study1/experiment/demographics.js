a167ca096304dd19c7dc0024bc7242e9a140dec2
Dominique Makowski <dom.mak19@gmail.com>
2024-09-04 16:54:34 +0100

    add expe

---
+++ study1/experiment/demographics.js
@@ -0,0 +1,68 @@
+// Consent Form =================================================================
+// The consent form is dynamically generated based on the URL variables.
+
+const ConsentForm = {
+    type: jsPsychSurvey,
+    survey_json: function () {
+        // Get URL variables
+        let urlvars = jsPsych.data.urlVariables()
+
+        // Logo and title
+        let text =
+            "<img src='https://blogs.brighton.ac.uk/sussexwrites/files/2019/06/University-of-Sussex-logo-transparent.png' width='150px' align='right'/><br><br><br><br><br>" +
+            "<h1>Informed Consent</h1>"
+
+        if (urlvars["exp"] == "surveyswap") {
+            text +=
+                "<p style='color:green;' align='left'><b>Note: You will receive a <i style='color:purple;'>SurveySwap.io</i> completion code at the end of the experiment.</b></p>"
+        }
+
+        // Main Text
+        text +=
+            // Overview
+            "<p align='left'><b>Invitation to Take Part</b><br>" +
+            "Thank you for considering to take part in this study conducted by Sofia Turcany Diaz and Dr Dominique Makowski from the University of Sussex (see contact information below).</p>" +
+            // Description
+            "<p align='left'><b>Why have I been invited and what will I do?</b><br>" +
+            "The goal is to study how new technology can impact <b>human perception</b>. In this study, you will be shown facial images and asked to complete a few questionnaires and perform some tasks. " +
+            "The whole experiment will take you <b style='color:#FF5722;'>~35 min</b> to complete. Please make you sure that you are <b>attentive and in a quiet environment</b>, and that you have time to complete it in one go.</p>" +
+            // Results and personal information
+            "<p align='left'><b>What will happen to the results and my personal information?</b><br>" +
+            "The results of this research may be written into a scientific publication. Your anonymity will be ensured in the way described in the consent information below. <b>Please read this information carefully</b> and then, if you wish to take part, please acknowledge that you have fully understood this sheet, and that you consent to take part in the study as it is described here.</p>" +
+            "<p align='left'><b>Consent</b><br></p>" +
+            // Bullet points
+            "<li align='left'>I understand that by signing below I am agreeing to take part in the University of Sussex research described here, and that I have read and understood this information sheet</li>" +
+            "<li align='left'>I understand that my participation is entirely voluntary, that I can choose not to participate in part or all of the study, and that I can withdraw at any stage without having to give a reason and without being penalized in any way (e.g., if I am a student, my decision whether or not to take part will not affect my grades).</li>" +
+            "<li align='left'>I understand that since the study is anonymous, it will be impossible to withdraw my data once I have completed it.</li>" +
+            "<li align='left'>I understand that my personal data will be used for the purposes of this research study and will be handled in accordance with Data Protection legislation. I understand that the University's Privacy Notice provides further information on how the University uses personal data in its research.</li>" +
+            "<li align='left'>I understand that my collected data will be stored in a de-identified way. De-identified data may be made publicly available through secured scientific online data repositories.</li>"
+
+        // Incentive
+        if (["surveyswap", "prolific"].includes(urlvars["exp"])) {
+            text +=
+                "<li align='left'>Please note that <b style='color:#FF5722;'>various checks will be performed to ensure the validity of the data</b>. We reserve the right to withhold credit awards or reimbursement should we detect non-valid responses (e.g., random patterns of answers, instructions not read, ...).</li>"
+        }
+
+        // End
+        text +=
+            "<li align='left'>By participating, you agree to follow the instructions and provide honest answers. If you do not wish to participate or if you don't have the time, simply close your browser.</li></p>" +
+            "<p align='left'><br><sub><sup>For further information about this research, or if you have any concerns, please contact Sofia Turcany Diaz (<i style='color:DodgerBlue;'>st633@sussex.ac.uk</i>) or Dr Dominique Makowski (<i style='color:DodgerBlue;'>D.Makowski@sussex.ac.uk</i>). This research has been approved (ER/ST633/1) by the ethics board of the School of Psychology. The University of Sussex has insurance in place to cover its legal liabilities in respect of this study.</sup></sub></p>"
+
+        // Return Survey
+        return {
+            showQuestionNumbers: false,
+            completeText: "I read, understood, and I consent",
+            pages: [
+                {
+                    elements: [
+                        {
+                            type: "html",
+                            name: "ConsentForm",
+                            html: text,
+                        },
+                    ],
+                },
+            ],
+        }
+    },
+}
---
+++ study1/experiment/intero.js
@@ -0,0 +1,195 @@
+// Instructions ================================================
+const intero_instructions = {
+    type: jsPsychSurvey,
+    survey_json: {
+        showQuestionNumbers: false,
+        completeText: "Let's start",
+        pages: [
+            {
+                elements: [
+                    {
+                        type: "html",
+                        name: "ConsentForm",
+                        html:
+                            "<div style='display: flex;'>" +
+                            "<div style='width: 60%; margin-right: 20px;'>" +
+                            "<h1>About you and your body...</h1>" +
+                            "<h2>Instructions</h2>" +
+                            "<p>In the following questionnaire, you will be asked various questions about the way you feel and you think about your body. " +
+                            "We are interested in understanding <b>what questions are related</b> for you as compared to other people. " +
+                            "There are no right or wrong answers.</p>" +
+                            "<p>We are aware that answering these questionnaires <b>might feel long and repetitive</b>, but having similar questions is necessary to ensure the validity of the results (we expect similar questions to be related). " +
+                            "Please read carefully each item and consider it <i>on its own</i> (<b>without trying to relate it or remember your answers to previous items</b>), and don't hesitate to take breaks if you need.</p>" +
+                            "<p style='color:green;'>At the end, you will be shown a <b>graph</b> summarizing your answers.<p>" +
+                            "</div>" +
+                            "<div style='width: 40%;'>" +
+                            "<img src='https://www.newthinking.com/wp-content/uploads/2023/06/2-ways-to-improve-mind-body-scaled.jpg' alt='Mind and Body' style='width: 100%;'>" +
+                            "</div>" +
+                            "</div>",
+                    },
+                ],
+            },
+        ],
+    },
+}
+
+// Items ================================================
+const intero_items = {
+    Sexual_State_Q1: "Sexual Arousal State 1",
+    Sexual_Cardiac_Q1: "Sexual Arousal Cardiac 1",
+    Sexual_Respiratory_Q1: "Sexual Arousal Respiratory 1",
+    Anxious_State_Q1: "Anxious Arousal State 1",
+    Anxious_Cardiac_Q1: "Anxious Arousal Cardiac 1",
+    Anxious_Respiratory_Q1: "Anxious Arousal Respiratory 1",
+}
+
+// Generation code ================================================
+// Convernience function to shuffle an object (used internally)
+function shuffleObject(obj) {
+    const entries = Object.entries(obj)
+    for (let i = entries.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1))
+        ;[entries[i], entries[j]] = [entries[j], entries[i]]
+    }
+    return Object.fromEntries(entries)
+}
+
+// This functions takes the items and groups them by pages depending on the condition
+function intero_makegroups(items, condition = "Domains") {
+    items = shuffleObject(items) // Shuffle items
+
+    if (condition == "Domains") {
+        groups = {
+            Sexual: [],
+            Anxious: [],
+        }
+
+        for (const k in items) {
+            groups[k.split("_")[0]].push({ [k]: items[k] })
+        }
+    } else if (condition == "Dimensions") {
+        groups = {
+            State: [],
+            Cardiac: [],
+            Respiratory: [],
+        }
+
+        for (const k in items) {
+            groups[k.split("_")[1]].push({ [k]: items[k] })
+        }
+    } else if (condition == "Random") {
+        groups = {
+            Page1: [],
+            Page2: [],
+            Page3: [],
+        }
+
+        // Max 2 items per page
+        for (const [index, k] of Object.keys(items).entries()) {
+            groups[`Page${Math.floor(index / 2) + 1}`].push({ [k]: items[k] })
+        }
+    }
+    return shuffleObject(groups)
+}
+
+// This function formats each question into a jsPsych-survey question that contains information about the question format
+function intero_makequestions(groups, required = true, ticks = ["Disagree", "Agree"]) {
+    pages = []
+
+    // Make questions
+    for (const g in groups) {
+        for (const [index, element] of groups[g].entries()) {
+            key = Object.keys(element)[0]
+            q = {
+                title: element[key],
+                name: key,
+                type: "rating",
+                displayMode: "buttons",
+                // scaleColorMode: "colored",
+                isRequired: required,
+                minRateDescription: ticks[0],
+                maxRateDescription: ticks[1],
+                rateValues: [0, 1, 2, 3, 4, 5, 6],
+            }
+            groups[g][index] = q
+        }
+        pages.push({ elements: groups[g] })
+    }
+
+    return pages
+}
+
+// Questionnaire ================================================
+// The condition is randomly selected when this script is read
+// const intero_condition = ["Domains", "Dimensions"][Math.floor(Math.random() * 2)]
+const intero_condition = "Random"
+
+const intero_questionnaire = {
+    type: jsPsychSurvey,
+    survey_json: function () {
+        return {
+            title: "About your personality",
+            description:
+                "Please answer the following questions based on how accurately each statement describes you in general.",
+            showQuestionNumbers: false,
+            goNextPageAutomatic: true,
+            pageNextText: "Next",
+            pagePrevText: "Previous",
+            showProgressBar: "aboveHeader",
+            pages: intero_makequestions(intero_makegroups(intero_items, intero_condition)),
+        }
+    },
+    data: {
+        screen: "intero_questionnaire",
+        condition: intero_condition,
+    },
+}
+
+// Make plot ========================================================================================================
+function intero_plotdata(screen) {
+    let data = jsPsych.data.get().filter({ screen: screen })
+    data = data["trials"][0]["response"]
+
+    // Compute average and rescale to percentage
+    cardiac = data["PA_State_Q1"] + data["NA_Cardiac_Q1"]
+    cardiac = (cardiac / 2 / 6) * 100
+
+    respiratory = data["PA_Respiratory_Q1"] + data["NA_Respiratory_Q1"]
+    respiratory = (respiratory / 2 / 6) * 100
+
+    state = data["PA_State_Q1"] + data["NA_State_Q1"]
+    state = (state / 2 / 6) * 100
+
+    // Prepare output
+    var output = {
+        names: ["Your Heart", "Your Breath", "State"],
+        scores: [cardiac, respiratory, state],
+        label: "Connectedness with each organ (%)",
+    }
+    return output
+}
+
+// Results
+const intero_feedback = {
+    type: jsPsychCanvasButtonResponse,
+    on_load: function () {
+        document.querySelector("canvas").style.removeProperty("display") // Force it to center
+    },
+    stimulus: function (c) {
+        let data = intero_plotdata("intero_questionnaire")
+        let ctx = c.getContext("2d")
+        let plot = new Chart(
+            ctx,
+            make_radarplot(
+                (names = data.names),
+                (scores = data.scores),
+                (minmax = [0, 100]),
+                (label = data.label),
+                (color = [0, 137, 123])
+            )
+        )
+    },
+    canvas_size: plot_getsize(),
+    choices: ["Continue"],
+    prompt: "<p>This chart represents how you relate to different body functions. Remember that it is neither good or bad to be high on any of these, and that there is no 'normal'. Everybody is different!</p>",
+}
