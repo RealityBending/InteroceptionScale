8a266e253570a5ff970896df665521f2c4a445b9
Ana Neves <asf25@sussex.ac.uk>
2024-11-11 16:17:47 +0000

    added pre_processing and rawdata file

---
+++ study1/analysis/0_preprocessing.R
@@ -0,0 +1,41 @@
+library(jsonlite)
+
+
+## path for data
+path <- "C:/Users/asf25/Box/InteroceptionScale/data"
+
+## JsPsych experiment ------------------------------------------------------
+
+files <- list.files(path, pattern = "*.csv")
+
+alldata <- data.frame()
+for (file in files) {
+  rawdata <- read.csv(paste0(path, "/", file))
+  
+  # Initialize participant-level data
+  dat <- rawdata[rawdata$screen == "browser_info",]
+  
+  data_ppt <- data.frame(Participant = dat$participantID,
+                         Recruitment = dat$researcher,
+                         Condition = dat$condition,
+                         Experiment_StartDate = as.POSIXct(paste(dat$date, dat$time), format = "%d/%m/%Y %H:%M:%S"),
+                         Experiment_Duration = rawdata[rawdata$screen == "demographics_debrief", "time_elapsed"] / 1000 / 60,
+                         Browser_Version = paste(dat$browser, dat$browser_version),
+                         Mobile = dat$mobile,
+                         Platform = dat$os,
+                         Screen_Width = dat$screen_width,
+                         Screen_Height = dat$screen_height)
+                       #  Refresh_Rate = dat$vsync_rate
+
+  
+  # Demographics
+  demog <- jsonlite::fromJSON(rawdata[rawdata$screen == "demographic_questions",]$response)
+  demog <- as.data.frame(t(demog))
+  demog$GenderOther <- NULL
+  data_ppt<- cbind(data_ppt, demog)
+}
+  
+# Save --------------------------------------------------------------------
+
+write.csv(data_ppt, "../data/rawdata_participants.csv", row.names = FALSE)
+
