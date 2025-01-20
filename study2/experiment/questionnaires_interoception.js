946f47347346daac5e318983d525a4fc04e7bc36
Dominique Makowski <dom.mak19@gmail.com>
2025-01-20 16:12:14 +0000

    add instruction line to BPQ

--- study2/experiment/questionnaires_interoception.js
+++ study2/experiment/questionnaires_interoception.js
@@ -4,41 +4,33 @@ const items_maia = {
     MAIA_Noticing_2: "I notice when I am uncomfortable in my body",
     MAIA_Noticing_3: "I notice where in my body I am comfortable",
     MAIA_Noticing_4: "I notice changes in my breathing, such as whether it slows down or speeds up",
-    MAIA_NotDistracting_1_R:
-        "I ignore physical tension or discomfort until they become more severe",
+    MAIA_NotDistracting_1_R: "I ignore physical tension or discomfort until they become more severe",
     MAIA_NotDistracting_2_R: "I distract myself from sensations of discomfort",
     MAIA_NotDistracting_3_R: "When I feel pain or discomfort, I try to power through it",
     MAIA_NotDistracting_4_R: "I try to ignore pain",
     MAIA_NotDistracting_5_R: "I push feelings of discomfort away by focusing on something",
-    MAIA_NotDistracting_6_R:
-        "When I feel unpleasant body sensations, I occupy myself with something else so I do not have to feel them",
+    MAIA_NotDistracting_6_R: "When I feel unpleasant body sensations, I occupy myself with something else so I do not have to feel them",
     MAIA_NotWorrying_1_R: "When I feel physical pain, I become upset",
     MAIA_NotWorrying_2_R: "I start to worry that something is wrong if I feel any discomfort",
     MAIA_NotWorrying_3: "I can notice an unpleasant body sensation without worrying about it",
     MAIA_NotWorrying_4: "I can stay calm and not worry when I have feelings of discomfort or pain",
     MAIA_NotWorrying_5_R: "When I am in discomfort or pain I cannot get it out of my mind",
-    MAIA_AttentionRegulation_1:
-        "I can pay attention to my breath without being distracted by things happening around me",
-    MAIA_AttentionRegulation_2:
-        "I can maintain awareness of my inner bodily sensations even when there is a lot going on around me",
-    MAIA_AttentionRegulation_3:
-        "When I am in conversation with someone, I can pay attention to my posture",
+    MAIA_AttentionRegulation_1: "I can pay attention to my breath without being distracted by things happening around me",
+    MAIA_AttentionRegulation_2: "I can maintain awareness of my inner bodily sensations even when there is a lot going on around me",
+    MAIA_AttentionRegulation_3: "When I am in conversation with someone, I can pay attention to my posture",
     MAIA_AttentionRegulation_4: "I can return awareness to my body if I am distracted",
     MAIA_AttentionRegulation_5: "I can refocus my attention from thinking to sensing my body",
-    MAIA_AttentionRegulation_6:
-        "I can maintain awareness of my whole body even when a part of me is in pain or discomfort",
+    MAIA_AttentionRegulation_6: "I can maintain awareness of my whole body even when a part of me is in pain or discomfort",
     MAIA_AttentionRegulation_7: "I am able to consciously focus on my body as a whole",
     MAIA_EmotionalAwareness_1: "I notice how my body changes when I am angry",
     MAIA_EmotionalAwareness_2: "When something is wrong in my life I can feel it in my body",
     MAIA_EmotionalAwareness_3: "I notice that my body feels different after a peaceful experience",
-    MAIA_EmotionalAwareness_4:
-        "I notice that my breathing becomes free and easy when I feel comfortable",
+    MAIA_EmotionalAwareness_4: "I notice that my breathing becomes free and easy when I feel comfortable",
     MAIA_EmotionalAwareness_5: "I notice how my body changes when I feel happy / joyful",
     MAIA_SelfRegulation_1: "When I feel overwhelmed I can find a calm place inside",
     MAIA_SelfRegulation_2: "When I bring awareness to my body I feel a sense of calm",
     MAIA_SelfRegulation_3: "I can use my breath to reduce tension",
-    MAIA_SelfRegulation_4:
-        "When I am caught up in thoughts, I can calm my mind by focusing on my body/breathing",
+    MAIA_SelfRegulation_4: "When I am caught up in thoughts, I can calm my mind by focusing on my body/breathing",
     MAIA_BodyListening_1: "I listen for information from my body about my emotional state",
     MAIA_BodyListening_2: "When I am upset, I take time to explore how my body feels",
     MAIA_BodyListening_3: "I listen to my body to inform me about what to do",
@@ -78,8 +70,7 @@ const questionnaire_maia = {
     survey_json: function () {
         return {
             title: "About you and your body",
-            description:
-                "Please indicate how often each statement applies to you generally in daily life.",
+            description: "Please indicate how often each statement applies to you generally in daily life.",
             showQuestionNumbers: false,
             goNextPageAutomatic: true,
             pages: make_maia(items_maia),
@@ -181,7 +172,13 @@ const items_bpq = {
 function make_bpq(items, required = true, ticks = ["Never", "Always"]) {
     items = shuffleObject(items)
 
-    questions = []
+    questions = [
+        {
+            type: "html",
+            name: "instructions_bpq",
+            html: "<p>During most situations, I am aware of the following...</p>",
+        },
+    ]
 
     // Make questions
     for (const key of Object.keys(items)) {
@@ -208,7 +205,8 @@ const questionnaire_bpq = {
     survey_json: function () {
         return {
             title: "Body awareness",
-            description: "Please rate your awareness on each of the characteristics described below. Select the answer that most accurately describes you.",
+            description:
+                "Please rate your awareness on each of the characteristics described below. Select the answer that most accurately describes you.",
             showQuestionNumbers: false,
             goNextPageAutomatic: true,
             pages: make_bpq(items_bpq),
